3e70ea60f9a9774b015263b700e62c853feb9750
sdavos <38280946+sdavos@users.noreply.github.com>
2020-08-26 12:44:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,9 @@ var greetingplaceholderSpan = document.querySelector("span#dc_greetings_template
    $(".article-vote-down").wrap('<div>Nous sommes navrés. Pouvez-vous nous indiquer la raison de ce vote négatif dans les commentaires ci-dessous ?</div>');
  });
 
-
+$(".article-vote-down").on('click', function() {
+   $(".article-vote-up, .article-vote-down, .article-votes-count, .article-votes-question").remove('');
+ });
 
 });
 
